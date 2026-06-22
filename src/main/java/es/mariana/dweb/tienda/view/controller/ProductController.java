0cd750f7346744ac43010f7fda65a1abfb0fd199
alba-tortosa <50136705+alba-tortosa@users.noreply.github.com>
2026-06-22 12:01:38 +0200

    th:if, #lists, th:ref, @RequestParam
    ThymeLeaf th:if, #lists, th:ref
    Spring @RequestParam
    
    (cherry picked from commit 989eb112d321e981fdb206a6488b9ed36a901cf3)

--- src/main/java/es/mariana/dweb/tienda/view/controller/ProductController.java
+++ src/main/java/es/mariana/dweb/tienda/view/controller/ProductController.java
@@ -25,6 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -41,5 +42,15 @@ public class ProductController {
         model.addAttribute("prods", allProducts);
         return "product/list";
     }
+
+    @GetMapping("product/comments")
+    public String orderList(@RequestParam("prodId") Integer prodId, Model model) {
+
+        final Product product = productService.findById(prodId);
+        model.addAttribute("prod", product);
+        return "product/comments";
+    }
+
+
 
 }
